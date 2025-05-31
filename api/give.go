bd40ac4622b3bb7a9fc9938067d133ab49b61338
Christian <Mxlightningx@gmail.com>
2025-05-30 21:34:02 -0500

    Configure gateway (for Vercel)

---
+++ api/give.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := "8080"
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.Static("/static", "../static")
+	router.StaticFile("/favicon.ico", "../static/favicon.ico")
+	router.GET("/", sponsorHandler)
+
+	fmt.Printf("Server listening on http://localhost:%s\n", port)
+	router.Run(":" + port)
+}
+
+func sponsorHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"stripe": "Thank you for your financial support!"})
+	// c.HTML(200, "give.html", gin.H{"stripe": "Thank you for your financial support!"})
+}
